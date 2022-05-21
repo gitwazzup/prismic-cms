da077ea46b0828fc8d1fe696281d7bbc80c8917d
Erik Sambell <erik@sambell.nl>
2022-05-21 06:36:57 +0200

    added custom type and slice

---
+++ .slicemachine/assets/slices/TextBlock/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TextBlock';
+
+export default {
+  title: 'slices/TextBlock'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"text_block","items":[],"primary":{"title":[{"type":"heading1","text":"Engage compelling supply-chains","spans":[]}],"description":[{"type":"paragraph","text":"Aute cillum qui pariatur.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
